bd3f1734d88051e5ce4d837299292db3ac546a9e
Pirate Seal <tcousin.pseal@gmail.com>
2019-10-22 11:47:58 +0200

    Server Init

---
+++ Server/src/test/java/fr/intech/roomies/ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.intech.roomies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
