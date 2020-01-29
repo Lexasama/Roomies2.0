2e50ffd117929b8d56fae61d80944258a21f3492
Pirate Seal <tcousin.pseal@gmail.com>
2020-01-29 18:50:59 +0100

    Increase / Decrease / Delete Item from Grocerylist

---
+++ Roomies2.0/src/Roomies2.DB/Scripts/4.StoredProcedures/1.GroceriesProcedures/3.Install-sDeleteGroceryListItem.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE rm2.sDeleteGroceryListItem
+( @ItemId  INT,
+  @GroceryListId INT
+) AS
+BEGIN
+    SET XACT_ABORT ON;
+    SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;
+    BEGIN TRAN;
+    
+    DELETE FROM rm2.itGroceryListItem WHERE GroceryListId = @GroceryListId AND ItemId = @ItemId;
+    COMMIT ;
+    RETURN 0;
+    
+END;
---
+++ Roomies2.0/src/Roomies2.DB/Scripts/4.StoredProcedures/1.GroceriesProcedures/4.Install-sDecreaseItemAmount.sql
@@ -0,0 +1,24 @@
+﻿CREATE PROCEDURE rm2.sDecreaseItemAmount
+( @ItemId        INT,
+  @GroceryListId INT
+) AS
+BEGIN
+    SET XACT_ABORT ON;
+    SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;
+    BEGIN TRAN;
+
+    IF (SELECT ItemAmount FROM rm2.itGroceryListItem WHERE GroceryListId = @GroceryListId AND ItemId = @ItemId > 1)
+            BEGIN
+                UPDATE rm2.itGroceryListItem
+                SET ItemAmount = ItemAmount - 1
+                    WHERE GroceryListId = @GroceryListId
+                      AND ItemId = @ItemId;
+                COMMIT;
+                RETURN 0;
+            END;
+        ELSE
+            BEGIN
+                EXECUTE rm2.sDeleteGroceryListItem @ItemId, @GroceryListId;
+            END
+END;
+GO;
